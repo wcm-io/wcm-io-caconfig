ea79c08f25bd312bf2fda0c7366d512c96667cf6
sseifert <sseifert@pro-vision.de>
2016-12-16 18:41:35 +0100

    make sure compat ApplicationImplementationPicker is used before the new one (sling models used reversed ranking order)

--- compat/src/main/java/io/wcm/config/core/impl/application/ApplicationImplementationPicker.java
+++ compat/src/main/java/io/wcm/config/core/impl/application/ApplicationImplementationPicker.java
@@ -36,7 +36,7 @@ import io.wcm.config.spi.annotations.Application;
  * but with support for the deprecated Application annotation.
  */
 @Component(immediate = true, service = ImplementationPicker.class, property = {
-    Constants.SERVICE_RANKING + ":Integer=1001"
+    Constants.SERVICE_RANKING + ":Integer=900"
 })
 @SuppressWarnings("deprecation")
 public class ApplicationImplementationPicker implements ImplementationPicker {
