426e89cf25b10b5e109f217d99044fcfceaa88b1
sseifert <sseifert@pro-vision.de>
2017-10-18 15:04:02 +0200

    fix code formatting and add file headers

--- extensions/src/main/java/io/wcm/caconfig/extensions/references/impl/ConfigurationReferenceProvider.java
+++ extensions/src/main/java/io/wcm/caconfig/extensions/references/impl/ConfigurationReferenceProvider.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2017 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package io.wcm.caconfig.extensions.references.impl;
 
 import java.util.ArrayList;
@@ -22,19 +41,19 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
 import com.day.cq.wcm.api.PageManagerFactory;
 import com.day.cq.wcm.api.reference.Reference;
 import com.day.cq.wcm.api.reference.ReferenceProvider;
 
 /**
  * <p>
- * This implementation of {@link ReferenceProvider} allows to resolve references of a given {@link Resource} to context-aware
- * configurations.
+ * This implementation of {@link ReferenceProvider} allows to resolve references of a given {@link Resource} to
+ * context-aware configurations.
  * </p>
  * <p>
- * This is for example used by ActivationReferenceSearchServlet to resolve referenced content of pages during activation of a page using
- * AEM sites. Returning the configurations allows the editor to activate them along with the page referring to them.
+ * This is for example used by ActivationReferenceSearchServlet to resolve referenced content of pages during activation
+ * of a page using AEM sites. Returning the configurations allows the editor to activate them along with the page
+ * referring to them.
  * </p>
  * <p>
  * This component can be disabled by configuration, but its enabled by default.
@@ -44,73 +63,75 @@ import com.day.cq.wcm.api.reference.ReferenceProvider;
 @Designate(ocd = ConfigurationReferenceProvider.Config.class)
 public class ConfigurationReferenceProvider implements ReferenceProvider {
 
-    @ObjectClassDefinition(name = "wcm.io Context-Aware Configuration Reference Provider",
-            description = "Allows to resolve references from resources to their Context-Aware configurations, for example during page activation.")
-    static @interface Config {
+  @ObjectClassDefinition(name = "wcm.io Context-Aware Configuration Reference Provider",
+      description = "Allows to resolve references from resources to their Context-Aware configurations, for example during page activation.")
+  static @interface Config {
 
-        @AttributeDefinition(name = "Enabled",
-                description = "Enable this reference provider.")
-        boolean enabled() default true;
+    @AttributeDefinition(name = "Enabled",
+        description = "Enable this reference provider.")
+    boolean enabled() default true;
+  }
+
+  @org.osgi.service.component.annotations.Reference
+  private ConfigurationManager configurationManager;
+
+  @org.osgi.service.component.annotations.Reference
+  private PageManagerFactory pageManagerFactory;
+
+  private boolean enabled = false;
+
+  @Activate
+  protected void activate(Config config) {
+    enabled = config.enabled();
+  }
+
+  @Deactivate
+  protected void deactivate() {
+    enabled = false;
+  }
+
+  @Override
+  public List<Reference> findReferences(Resource resource) {
+    if (!enabled) {
+      return Collections.emptyList();
     }
 
-    @org.osgi.service.component.annotations.Reference
-    private ConfigurationManager configurationManager;
+    Set<String> configurationNames = configurationManager.getConfigurationNames();
+    List<Reference> references = new ArrayList<>(configurationNames.size());
+    ResourceResolver resourceResolver = resource.getResourceResolver();
 
-    @org.osgi.service.component.annotations.Reference
-    private PageManagerFactory pageManagerFactory;
+    for (String configurationName : configurationNames) {
+      ConfigurationData configurationData = configurationManager.getConfiguration(resource, configurationName);
+      ConfigurationMetadata configurationMetadata = configurationManager.getConfigurationMetadata(configurationName);
+      Resource configurationResource = resourceResolver.getResource(configurationData.getResourcePath());
 
-    private boolean enabled = false;
-
-    @Activate
-    protected void activate(Config config) {
-        enabled = config.enabled();
+      if (configurationResource != null) {
+        references.add(new Reference(getType(), StringUtils.defaultIfEmpty(configurationMetadata.getLabel(),
+            configurationMetadata.getName()), configurationResource, getLastModifiedOf(configurationResource)));
+      }
     }
 
-    @Deactivate
-    protected void deactivate() {
-        enabled = false;
+    return references;
+  }
+
+  private long getLastModifiedOf(Resource configurationResource) {
+    Page configurationPage = configurationResource.adaptTo(Page.class);
+
+    if (configurationPage == null && StringUtils.equals(configurationResource.getName(), JcrConstants.JCR_CONTENT)) {
+      configurationPage = configurationResource.getParent().adaptTo(Page.class);
     }
 
-    @Override
-    public List<Reference> findReferences(Resource resource) {
-        if (!enabled) {
-            return Collections.emptyList();
-        }
-
-        Set<String> configurationNames = configurationManager.getConfigurationNames();
-        List<Reference> references = new ArrayList<>(configurationNames.size());
-        ResourceResolver resourceResolver = resource.getResourceResolver();
-
-        for(String configurationName : configurationNames) {
-            ConfigurationData configurationData = configurationManager.getConfiguration(resource, configurationName);
-            ConfigurationMetadata configurationMetadata = configurationManager.getConfigurationMetadata(configurationName);
-            Resource configurationResource = resourceResolver.getResource(configurationData.getResourcePath());
-
-            if (configurationResource != null) {
-                references.add(new Reference(getType(), StringUtils.defaultIfEmpty(configurationMetadata.getLabel(),
-                        configurationMetadata.getName()), configurationResource, getLastModifiedOf(configurationResource)));
-            }
-        }
-
-        return references;
+    if (configurationPage != null && configurationPage.getLastModified() != null) {
+      return configurationPage.getLastModified().getTimeInMillis();
     }
-
-    private long getLastModifiedOf(Resource configurationResource) {
-        Page configurationPage = configurationResource.adaptTo(Page.class);
-
-        if (configurationPage == null && StringUtils.equals(configurationResource.getName(), JcrConstants.JCR_CONTENT)) {
-            configurationPage = configurationResource.getParent().adaptTo(Page.class);
-        }
-
-        if (configurationPage != null && configurationPage.getLastModified() != null) {
-            return configurationPage.getLastModified().getTimeInMillis();
-        } else {
-            ValueMap properties = configurationResource.getValueMap();
-            return properties.get(JcrConstants.JCR_LASTMODIFIED, Calendar.class).getTimeInMillis();
-        }
+    else {
+      ValueMap properties = configurationResource.getValueMap();
+      return properties.get(JcrConstants.JCR_LASTMODIFIED, Calendar.class).getTimeInMillis();
     }
+  }
 
-    private static String getType() {
-        return "caconfig";
-    }
+  private static String getType() {
+    return "caconfig";
+  }
+
 }
--- extensions/src/test/java/io/wcm/caconfig/extensions/references/impl/ConfigurationReferenceProviderTest.java
+++ extensions/src/test/java/io/wcm/caconfig/extensions/references/impl/ConfigurationReferenceProviderTest.java
@@ -1,15 +1,31 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2017 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package io.wcm.caconfig.extensions.references.impl;
 
 import static org.apache.sling.testing.mock.caconfig.ContextPlugins.CACONFIG;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NavigableMap;
 import java.util.SortedSet;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
 import javax.annotation.CheckForNull;
@@ -43,95 +59,97 @@ import io.wcm.testing.mock.aem.junit.AemContextCallback;
 
 public class ConfigurationReferenceProviderTest {
 
-    @Rule
-    public AemContext aemContext = new AemContextBuilder()
-            .beforeSetUp(new AemContextCallback() {
-                @Override
-                public void execute(AemContext ctx) {
-                    // also find sling:configRef props in cq:Page/jcr:content nodes
-                    MockOsgi.setConfigForPid(ctx.bundleContext(), "org.apache.sling.caconfig.resource.impl.def.DefaultContextPathStrategy",
-                            "configRefResourceNames", new String[] { "jcr:content", "." });
-                }
-            })
-            .plugin(CACONFIG)
-            .build();
-
-    private static final ValueMap configurationA = new ValueMapDecorator(ImmutableMap.of("key", "foo"));
-    private static final ValueMap configurationB = new ValueMapDecorator(ImmutableMap.of("key", "bar"));
-
-
-
-    @Before
-    public void setup() {
-        aemContext.create().resource("/conf");
-        Page page1 = aemContext.create().page("/content/site1/page", "pagetemplate", ImmutableMap.of("sling:configRef", "/conf/test/site1"));
-        Page page2 = aemContext.create().page("/content/site2/page", "pagetemplate", ImmutableMap.of("sling:configRef", "/conf/test/site2"));
-
-        TestConfigurationMetadataProvider metadataProvider = new TestConfigurationMetadataProvider();
-        metadataProvider.addConfigurationClass(ConfigurationA.class);
-        metadataProvider.addConfigurationClass(ConfigurationB.class);
-
-        aemContext.registerInjectActivateService(new PagePersistenceStrategy(), "enabled", true);
-        aemContext.registerService(PageManagerFactory.class, new PageManagerFactory() {
-            @Override public PageManager getPageManager(ResourceResolver resourceResolver) {
-                return aemContext.pageManager();
-            }
-        });
-        aemContext.registerService(ConfigurationMetadataProvider.class, metadataProvider);
-
-        applyConfig(page1, "configA", configurationA); // 1 config on page1
-        applyConfig(page2, "configA", configurationA); // 2 configs on page2
-        applyConfig(page2, "configB", configurationB);
-    }
-
-    @Test
-    public void testReferencesOfPage1() {
-        ReferenceProvider referenceProvider = new ConfigurationReferenceProvider();
-        aemContext.registerInjectActivateService(referenceProvider);
-        List<Reference> references = referenceProvider.findReferences(aemContext.resourceResolver().getResource("/content/site1/page"));
-        assertEquals(1, references.size());
-    }
-
-    @Test
-    public void testReferencesOfPage2() {
-        ReferenceProvider referenceProvider = new ConfigurationReferenceProvider();
-        aemContext.registerInjectActivateService(referenceProvider);
-        List<Reference> references = referenceProvider.findReferences(aemContext.resourceResolver().getResource("/content/site2/page"));
-        assertEquals(2, references.size());
-    }
-
-    @Test
-    public void testDisabled() {
-        ReferenceProvider referenceProvider = new ConfigurationReferenceProvider();
-        aemContext.registerInjectActivateService(referenceProvider, "enabled", false);
-        List<Reference> references = referenceProvider.findReferences(aemContext.resourceResolver().getResource("/content/site1/page"));
-        assertEquals(0, references.size());
-    }
-
-    private void applyConfig(Page p, String name, ValueMap values) {
-        ConfigurationManager configManager = aemContext.getService(ConfigurationManager.class);
-        Resource contextResource = p.adaptTo(Resource.class);
-        configManager.persistConfiguration(contextResource, name, new ConfigurationPersistData(values));
-    }
-
-    private static class TestConfigurationMetadataProvider implements ConfigurationMetadataProvider {
-
-        private final Map<String, ConfigurationMetadata> metadata = new HashMap<>();
-
-        void addConfigurationClass(Class<?> cls) {
-            metadata.put(AnnotationClassParser.getConfigurationName(cls), AnnotationClassParser.buildConfigurationMetadata(cls));
-        }
-
-        @Nonnull
+  @Rule
+  public AemContext aemContext = new AemContextBuilder()
+      .beforeSetUp(new AemContextCallback() {
         @Override
-        public SortedSet<String> getConfigurationNames() {
-            return new TreeSet<>(metadata.keySet());
+        public void execute(AemContext ctx) {
+          // also find sling:configRef props in cq:Page/jcr:content nodes
+          MockOsgi.setConfigForPid(ctx.bundleContext(), "org.apache.sling.caconfig.resource.impl.def.DefaultContextPathStrategy",
+              "configRefResourceNames", new String[] { "jcr:content", "." });
         }
+      })
+      .plugin(CACONFIG)
+      .build();
 
-        @CheckForNull
-        @Override
-        public ConfigurationMetadata getConfigurationMetadata(String s) {
-            return metadata.get(s);
-        }
+  private static final ValueMap configurationA = new ValueMapDecorator(ImmutableMap.of("key", "foo"));
+  private static final ValueMap configurationB = new ValueMapDecorator(ImmutableMap.of("key", "bar"));
+
+
+  @Before
+  public void setup() {
+    aemContext.create().resource("/conf");
+    Page page1 = aemContext.create().page("/content/site1/page", "pagetemplate", ImmutableMap.of("sling:configRef", "/conf/test/site1"));
+    Page page2 = aemContext.create().page("/content/site2/page", "pagetemplate", ImmutableMap.of("sling:configRef", "/conf/test/site2"));
+
+    TestConfigurationMetadataProvider metadataProvider = new TestConfigurationMetadataProvider();
+    metadataProvider.addConfigurationClass(ConfigurationA.class);
+    metadataProvider.addConfigurationClass(ConfigurationB.class);
+
+    aemContext.registerInjectActivateService(new PagePersistenceStrategy(), "enabled", true);
+    aemContext.registerService(PageManagerFactory.class, new PageManagerFactory() {
+
+      @Override
+      public PageManager getPageManager(ResourceResolver resourceResolver) {
+        return aemContext.pageManager();
+      }
+    });
+    aemContext.registerService(ConfigurationMetadataProvider.class, metadataProvider);
+
+    applyConfig(page1, "configA", configurationA); // 1 config on page1
+    applyConfig(page2, "configA", configurationA); // 2 configs on page2
+    applyConfig(page2, "configB", configurationB);
+  }
+
+  @Test
+  public void testReferencesOfPage1() {
+    ReferenceProvider referenceProvider = new ConfigurationReferenceProvider();
+    aemContext.registerInjectActivateService(referenceProvider);
+    List<Reference> references = referenceProvider.findReferences(aemContext.resourceResolver().getResource("/content/site1/page"));
+    assertEquals(1, references.size());
+  }
+
+  @Test
+  public void testReferencesOfPage2() {
+    ReferenceProvider referenceProvider = new ConfigurationReferenceProvider();
+    aemContext.registerInjectActivateService(referenceProvider);
+    List<Reference> references = referenceProvider.findReferences(aemContext.resourceResolver().getResource("/content/site2/page"));
+    assertEquals(2, references.size());
+  }
+
+  @Test
+  public void testDisabled() {
+    ReferenceProvider referenceProvider = new ConfigurationReferenceProvider();
+    aemContext.registerInjectActivateService(referenceProvider, "enabled", false);
+    List<Reference> references = referenceProvider.findReferences(aemContext.resourceResolver().getResource("/content/site1/page"));
+    assertEquals(0, references.size());
+  }
+
+  private void applyConfig(Page p, String name, ValueMap values) {
+    ConfigurationManager configManager = aemContext.getService(ConfigurationManager.class);
+    Resource contextResource = p.adaptTo(Resource.class);
+    configManager.persistConfiguration(contextResource, name, new ConfigurationPersistData(values));
+  }
+
+  private static class TestConfigurationMetadataProvider implements ConfigurationMetadataProvider {
+
+    private final Map<String, ConfigurationMetadata> metadata = new HashMap<>();
+
+    void addConfigurationClass(Class<?> cls) {
+      metadata.put(AnnotationClassParser.getConfigurationName(cls), AnnotationClassParser.buildConfigurationMetadata(cls));
     }
+
+    @Nonnull
+    @Override
+    public SortedSet<String> getConfigurationNames() {
+      return new TreeSet<>(metadata.keySet());
+    }
+
+    @CheckForNull
+    @Override
+    public ConfigurationMetadata getConfigurationMetadata(String s) {
+      return metadata.get(s);
+    }
+  }
+
 }
